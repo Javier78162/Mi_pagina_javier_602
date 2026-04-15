90dc4b8960ab3210265ec438c2ae75145202ab87
Javier78162 <javiergomezberprini@gmail.com>
2026-04-15 22:21:34 +0800

    hola

--- Script.js
+++ Script.js
@@ -1,9 +1,9 @@
-funtion mostar mensaje (){
+function mostarMensaje (){
     let mensaje=document.getElementById("mostrar mensaje");
     mensaje .innerHTML="🐾 cuida y  protegue a los perros";
     mensaje.style.display="block";
     setTimeout(c)=>{
         mensaje.style.opacity="1";
         mensaje.style.transform="translate Y(0)";
-    },50;
+    }50;
 }

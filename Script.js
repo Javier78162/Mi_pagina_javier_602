484e58f60cff89a9c1b545908d359953ab057caa
Javier78162 <javiergomezberprini@gmail.com>
2026-04-21 04:22:49 +0800

    HOLA

--- Script.js
+++ Script.js
@@ -1,19 +1,25 @@
-function mostarMensaje (){
-    let mensaje=document.getElementById("sobre_mi");
-    mensaje .InnerHTML ="🐾 cuida y  protegue a los perros";
-    mensaje.style.display ="black";
+scrip
 
-    setTimeout(() => {
-        mensaje.style.opacity = "1";
-        mensaje.style.transform = "translateY(0)";
-    },50);
+function da_click (){
+    let mensaje=document.getElementById("sobre_mi");
+    mensaje.innerHTML ="🐾Cuida y proteje a los animales";
+    mensaje.style.display = "black";
+
+ setTimeout (() => {
+    mensaje.style.opacity = "1";
+    mensaje.style.transform = "transformateY/(0)";
+  }, 50);
 }
-function mostar_mensaje (){
-    let mensaje=document.getElementById("mostrar mensaje");
-    mensaje .InnerHTML ="🐾 cuida y  protegue a los perros";
-    mensaje.style.display ="block";
-    setTimeout(()=> {
-        mensaje.style.opacity = "1";
-        mensaje.style.transform = "translateY(0)";
-    },50);
+ 
+
+function mostrar_mensaje (){
+    let mensaje=document.getElementById("mensaje");
+    mensaje.innerHTML ="🐾Cuida,proteje a los animales";
+    mensaje.style.display = "black";
+
+ setTimeout (() => {
+    mensaje.style.opacity = "1";
+    mensaje.style.transform = "transformateY/(0)";
+ }, 50);
 }
+ 
